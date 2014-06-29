2de9bb1e0af615d79a43cc5651b5b76f916d899a
Bertrik Sikken <bertrik@sikken.nl>
2014-06-29 19:10:31 +0200

    Add led banner simulator

---
+++ sim/README.md
@@ -0,0 +1,15 @@
+ledbanner simulator
+===================
+
+This program simulates the led banner, taking input from STDIN just like the
+actual ledbanner program, but outputting to a window created using SDL.
+
+Display is 10 times as large (800x80 pixels) so it fits nicely on a typical
+desktop screen.
+
+Requirements:
+    libsdl2-dev
+    
+To build it:
+    make
+
---
+++ sim/ledbannersim.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include "SDL.h"
+#include "SDL_video.h"
+
+#define WIDTH 80
+#define HEIGHT 8
+#define DEPTH 24
+#define PIXELS (WIDTH * HEIGHT)
+#define BYTES (PIXELS * 3)
+
+#define BIGGER  10
+
+static uint8_t frame[HEIGHT][WIDTH][3];
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = 
+        SDL_CreateWindow("ledbanner", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH*BIGGER, HEIGHT*BIGGER, 0);
+    SDL_Surface *surface = SDL_GetWindowSurface(window);
+
+    // main loop
+    int c;
+    while ((c = read(0, frame, BYTES))) {
+    
+        SDL_Surface *frame_surface = 
+            SDL_CreateRGBSurfaceFrom(frame, WIDTH, HEIGHT, DEPTH, WIDTH*3, 0x0000FF, 0x00FF00, 0xFF0000, 0);
+        SDL_BlitScaled(frame_surface, NULL, surface, NULL);
+        SDL_UpdateWindowSurface(window);
+        
+        SDL_Event event;
+        if (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                printf("Quit received...");
+                break;
+            }
+        }
+    }    
+    
+    SDL_Quit();
+    printf("Quit!\n");
+    return 0;
+}
+
