c9f87481cbbe07ef5cac5bd11c0b381cd38c1c8c
Bertrik Sikken <bertrik@sikken.nl>
2014-06-30 18:41:30 +0200

    Do not create a surface for each frame, but only once. Also fixes memory leak.

--- sim/ledbannersim.c
+++ sim/ledbannersim.c
@@ -23,16 +23,19 @@ int main(int argc, char *argv[])
 
     SDL_Init(SDL_INIT_VIDEO);
 
+    // get window surface
     SDL_Window *window = 
         SDL_CreateWindow("ledbanner", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH*BIGGER, HEIGHT*BIGGER, 0);
     SDL_Surface *surface = SDL_GetWindowSurface(window);
 
+    // create surface for led banner frame
+    SDL_Surface *frame_surface = 
+        SDL_CreateRGBSurfaceFrom(frame, WIDTH, HEIGHT, DEPTH, WIDTH*3, 0x0000FF, 0x00FF00, 0xFF0000, 0);
+
     // main loop
     int c;
     while ((c = read(0, frame, BYTES))) {
-    
-        SDL_Surface *frame_surface = 
-            SDL_CreateRGBSurfaceFrom(frame, WIDTH, HEIGHT, DEPTH, WIDTH*3, 0x0000FF, 0x00FF00, 0xFF0000, 0);
+
         SDL_BlitScaled(frame_surface, NULL, surface, NULL);
         SDL_UpdateWindowSurface(window);
         
@@ -45,6 +48,7 @@ int main(int argc, char *argv[])
         }
     }    
     
+    SDL_FreeSurface(frame_surface);
     SDL_Quit();
     printf("Quit!\n");
     return 0;
